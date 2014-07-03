aff2cbf410cd24bb926f282be87b493488e9c162
xiaoxiaogao <915288796@qq.com>
2014-07-03 18:12:47 +0800

    1
    1

---
+++ MyPdf/AppConstant.h
@@ -0,0 +1,22 @@
+//
+//  AppConstant.h
+//  read
+//
+//  Created by gao wenjian on 14-7-3.
+//
+//
+
+#ifndef read_AppConstant_h
+#define read_AppConstant_h
+
+#define ROOT_URL @"http://www.uki365.com/"
+
+#define GET_CATEGORY_LIST @"GetCategoryList.php"
+
+//不加参数，表示获取全部
+//GetProList.php?CateId=15&Page=1&Limit=10
+#define GET_PROLIST @"GetProList.php"
+
+
+
+#endif
